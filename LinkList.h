a930fbd99d28bfac2ff9bbc3ff28d6cf49401d60
ZhixuanLi <zhixuanlee2002@163.com>
2025-09-13 19:59:45 +0800

    Routine upgrade

--- LinkList.h
+++ LinkList.h
@@ -33,4 +33,14 @@ public:
 	bool make_example(); // 生成一段展示性链表
 };
 
-std::ostream& operator<<(std::ostream& out, LinkList& list); // 重载输出流
+std::ostream& operator<<(std::ostream& out, LinkList& list); // 重载输出流 
+
+/*
+类模板与函数模板的区别：(deprecated)
+1. 类模板不支持隐式类型推导，需要显式指明类型(before C++17)
+2. 类模板支持默认模板类型，函数模板则无默认(before C++11) 
+由于使用的C++17标准，所以就当我没说过，注意版本号即可 
+
+模板类和普通类的区别：
+模板类只在被调用时被创建；普通类在一开始编译时就被创建。
+*/
